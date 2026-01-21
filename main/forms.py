c2059881b4ea542fa52867c2551125cab3ba2044
lonayr <145482415+lonayr@users.noreply.github.com>
2026-01-21 20:55:00 +0300

    Delete main directory

--- main/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Course
-
-class CourseEditForm(forms.ModelForm):
-    class Meta:
-        model = Course
-        fields = ('title', 'description', 'thumbnail', 'featured_video', 'level', 'duration', 'category', 'requirements', 'content', 'lesson_title', 'lesson_video')
